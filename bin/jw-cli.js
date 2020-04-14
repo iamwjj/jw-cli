57ec176f4917a69656480305172b220113ed6fa7
iamwjj <793729222@qq.com>
2020-04-14 23:38:01 +0800

    feat: add 'init' command

--- bin/jw-cli.js
+++ bin/jw-cli.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 const program = require('commander')
 
+program.version('0.1.0', '-v, --version', '当前CLI版本号')
+
+program
+  .command('init <name>')
+  .description('初始化一个项目')
+  .action((name) => {
+    console.log('创建一个项目：' + name)
+  })
+
+program.parse(process.argv)
