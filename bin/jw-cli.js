c3658e43c0af04a5134add2d2320dfff9273ffb8
iamwjj <793729222@qq.com>
2020-04-15 23:29:01 +0800

    feat: 拉取模板功能

--- bin/jw-cli.js
+++ bin/jw-cli.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 const program = require('commander')
+const download = require('download-git-repo')
+const ora = require('ora')
+const chalk = require('chalk')
+
+const spinner = ora('正在拉取模板...')
 
 program.version('0.1.0', '-v, --version', '当前CLI版本号')
 
@@ -7,7 +12,15 @@ program
   .command('init <name>')
   .description('初始化一个项目')
   .action((name) => {
-    console.log('创建一个项目：' + name)
+    spinner.start()
+    download('iamwjj/vue-template', name, { clone: true }, (err) => {
+      spinner.stop()
+      if(!err) {
+        console.log('成功创建项目 ' + chalk.yellow(name))
+      } else {
+        console.log(err)
+      }
+    })
   })
 
 program.parse(process.argv)
