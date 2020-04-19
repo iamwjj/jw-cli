c035c339cedbb4fae87f233ebfd0c31a17ce7185
iamwjj <793729222@qq.com>
2020-04-19 18:26:04 +0800

    feat: 可选择模板

--- bin/jw-cli.js
+++ bin/jw-cli.js
@@ -3,6 +3,7 @@ const program = require('commander')
 const download = require('download-git-repo')
 const ora = require('ora')
 const chalk = require('chalk')
+const inquirer = require('inquirer')
 
 const spinner = ora('正在拉取模板...')
 
@@ -12,14 +13,29 @@ program
   .command('init <name>')
   .description('初始化一个项目')
   .action((name) => {
-    spinner.start()
-    download('iamwjj/vue-template', name, { clone: true }, (err) => {
-      spinner.stop()
-      if(!err) {
-        console.log('成功创建项目 ' + chalk.yellow(name))
+    inquirer.prompt([{
+      type: 'list',
+      name: 'projectType',
+      message: '你需要创建什么项目？',
+      choices: ['Vue', 'React', '多页面']
+    }]).then(({ projectType }) => {
+      let tplUrl = ''
+      if(projectType === 'Vue') {
+        tplUrl = 'iamwjj/vue-template'
+      } else if(projectType === 'React') {
+        tplUrl = 'iamwjj/react-template'
       } else {
-        console.log(err)
+        tplUrl = 'iamwjj/multiple-page-template'
       }
+      spinner.start()
+      download(tplUrl, name, { clone: true }, (err) => {
+        spinner.stop()
+        if(!err) {
+          console.log('成功创建' + projectType + '项目 ' + chalk.yellow(name))
+        } else {
+          console.log(err)
+        }
+      })
     })
   })
 
